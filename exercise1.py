45e1601e957d70c9a312191b32028b036a7bedea
Jiaxi Zeng <zengjx@utexas.edu>
2026-01-25 23:07:00 +0000

    remove extra ex1

--- exercise1.py
+++
@@ -1,8 +0,0 @@
-nub_list = [20,31,23,41,44,54,36,57,90,11]
-def even_or_odd(n):
-    if n % 2 == 0:
-        return "even"
-    else:
-        return "odd"
-for num in nub_list:
-    print(num, even_or_odd(num))
