43d2b667d816fee37974d6e8598e486808b7c7c8
Jiaxi Zeng <zengjx@utexas.edu>
2026-02-02 17:18:17 +0000

    Add exercise3 and xml

---
+++ homework03/exercise3.py
@@ -0,0 +1,14 @@
+import json
+import xmltodict
+
+with open("proteinlist_hw3.json", "r") as f:
+    data = json.load(f)
+
+
+root = {}
+root["proteins"] = {}
+root["proteins"]["protein"] = data["protein_list"]
+
+
+with open("proteins.xml", "w") as o:
+    o.write(xmltodict.unparse(root, pretty=True))
