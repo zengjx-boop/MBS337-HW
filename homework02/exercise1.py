7f1bf595349ea2b81083fb9a1767774217c69fe8
Jiaxi Zeng <zengjx@utexas.edu>
2026-02-02 16:38:45 +0000

    Update homework02 exercise1

--- homework02/exercise1.py
+++ homework02/exercise1.py
@@ -1,8 +1,8 @@
-nub_list = [20,31,23,41,44,54,36,57,90,11]
+num_list = [20,31,23,41,44,54,36,57,90,11]
 def even_or_odd(n):
     if n % 2 == 0:
         return "even"
     else:
         return "odd"
-for num in nub_list:
+for num in num_list:
     print(num, even_or_odd(num))
